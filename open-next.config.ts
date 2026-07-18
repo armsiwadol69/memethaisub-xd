e53a89b750b423bbb6435288e0ee27009328f04c
armsiwadol69 <armsiwadol2543@gmail.com>
2026-07-19 02:06:48 +0700

    chore: remove obsolete open-next.config.ts configuration file

--- open-next.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
-
-export default defineCloudflareConfig({
-    // Overrides the default Next.js cache to use Cloudflare R2
-    incrementalCache: r2IncrementalCache,
-});
